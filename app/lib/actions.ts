63ab727da20796df0d964336ad74a10a5409884d
Fernanda Vaz <flauta.fernanda@gmail.com>
2025-02-21 21:44:53 -0300

    chapter 15 completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,30 @@
 'use server'
+
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import postgres from 'postgres'
 import { z } from 'zod'
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.'
+        default:
+          return 'Something went wrong'
+      }
+    }
+    throw error
+  }
+}
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
@@ -100,8 +122,12 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice')
+  // throw new Error('Failed to Delete Invoice')
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`
+  } catch (error) {
+    console.error(error)
+  }
 
-  await sql`DELETE FROM invoices WHERE id = ${id}`
   revalidatePath('/dashboard/invoices')
 }
